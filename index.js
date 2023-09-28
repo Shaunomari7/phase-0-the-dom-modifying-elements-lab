662971a4c95a35cd8c8c5eb7d6b9367e582c73a7
Shaun Omari <Shaunomari72@gmail.com>
2023-09-28 21:02:05 +0300

    lab done

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+let noMain = document.querySelector('main')
+noMain.remove()
+
+let newHeader = document.createElement("h1")
+newHeader.textContent = "#victory"
+document.body.append(newHeader)
+
