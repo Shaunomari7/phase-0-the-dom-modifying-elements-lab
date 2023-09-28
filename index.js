99e8b89a02c639a2b21c86af09ab6f94650e58e6
Shaun Omari <Shaunomari72@gmail.com>
2023-09-28 21:15:07 +0300

    updated lab

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ let noMain = document.querySelector('main')
 noMain.remove()
 
 let newHeader = document.createElement("h1")
-newHeader.textContent = "#victory"
+newHeader.id = "victory"
+newHeader.innerText= "SHAUN is the champion"
 document.body.append(newHeader)
 
